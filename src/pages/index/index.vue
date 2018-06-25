c0292561ca074b3c208bd0e1af94c9f22e9d5359
zuolinya <zuolinya3@zuolinyadeMacBook-Pro.local>
2018-06-25 19:27:35 +0800

    功能：初始化项目

---
+++ src/pages/index/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+
+    <div class="userinfo" @click="bindViewTap">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <div class="userinfo-nickname">
+        <card :text="userInfo.nickName"></card>
+      </div>
+    </div>
+
+    <div class="usermotto">
+      <div class="user-motto">
+        <card :text="motto"></card>
+      </div>
+    </div>
+
+    <form class="form-container">
+      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
+    </form>
+    <a href="/pages/counter/main" class="counter">初始化</a>
+  </div>
+</template>
+
+<script>
+import card from "@/components/card";
+
+export default {
+  data() {
+    return {
+      motto: "Hello World",
+      userInfo: {}
+    };
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap() {
+      const url = "../logs/main";
+      wx.navigateTo({ url });
+    },
+    getUserInfo() {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: res => {
+              this.userInfo = res.userInfo;
+            }
+          });
+        }
+      });
+    },
+    clickHandle(msg, ev) {
+      console.log("clickHandle:", msg, ev);
+    }
+  },
+
+  created() {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo();
+  }
+};
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.userinfo-avatar {
+  margin: 20rpx;
+  width: 128rpx;
+  height: 128rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  margin-bottom: 5px;
+  padding: 0 12px;
+  border: 1px solid #ccc;
+}
+
+.counter {
+  display: inline-block;
+  margin: 10px auto;
+  padding: 5px 10px;
+  border: 1px solid blue;
+  color: blue;
+}
+</style>
